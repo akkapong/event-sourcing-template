5e6874c9156d80c113ff6c3dd1a3fa964cb60725
Akkapong Kajornwongwattana <akk.ohm@gmail.com>
2017-11-28 17:24:03 +0700

    remove comment

--- script/generate_event.php
+++ script/generate_event.php
@@ -13,15 +13,15 @@ $generator->createDirectory();
 
 
 //generate collection
-// $generator->getGenClass('collection')->createFile();
-// $generator->getGenClass('infrastructure')->createFile();
-// $generator->getGenClass('command')->createFile();
-// $generator->getGenClass('command_handle')->createFile();
-// $generator->getGenClass('event')->createFile();
-// $generator->getGenClass('module')->createFile();
-// $generator->getGenClass('module_repo')->createFile();
-// $generator->getGenClass('projection')->createFile();
-// $generator->getGenClass('controller')->createFile();
-// $generator->getGenClass('worker')->createFile();
-// $generator->getGenClass('command_route')->createFile();
-// $generator->getGenClass('event_route')->createFile();
+$generator->getGenClass('collection')->createFile();
+$generator->getGenClass('infrastructure')->createFile();
+$generator->getGenClass('command')->createFile();
+$generator->getGenClass('command_handle')->createFile();
+$generator->getGenClass('event')->createFile();
+$generator->getGenClass('module')->createFile();
+$generator->getGenClass('module_repo')->createFile();
+$generator->getGenClass('projection')->createFile();
+$generator->getGenClass('controller')->createFile();
+$generator->getGenClass('worker')->createFile();
+$generator->getGenClass('command_route')->createFile();
+$generator->getGenClass('event_route')->createFile();
